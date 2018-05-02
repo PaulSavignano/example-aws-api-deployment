1abb034e48779e041ad9b080d5dbd7c91be76948
Paul Savignano <Paul.Savignano@gmail.com>
2018-05-02 14:42:48 -0700

    Add route for pages update-key

--- src/routes/pages.js
+++ src/routes/pages.js
@@ -7,6 +7,7 @@ import {
   get,
   remove,
   update,
+  updateKey,
   updateName,
   updateOrder,
   updateSections,
@@ -18,10 +19,11 @@ const pages = express.Router()
 pages.delete('/:_id', authenticate(['admin']), catchErrors(remove))
 pages.get('/', catchErrors(get))
 pages.patch('/:_id', authenticate(['admin']), catchErrors(update))
+pages.patch('/:_id/update-key', authenticate(['admin']), catchErrors(updateKey))
+pages.patch('/:_id/update-name', authenticate(['admin']), catchErrors(updateName))
 pages.patch('/:_id/update-order', authenticate(['admin']), catchErrors(updateOrder))
 pages.patch('/:_id/update-sections', authenticate(['admin']), catchErrors(updateSections))
 pages.patch('/:_id/update-value', authenticate(['admin']), catchErrors(updateValue))
-pages.patch('/:_id/update-name', authenticate(['admin']), catchErrors(updateName))
 pages.post('/', authenticate(['admin']), catchErrors(add))
 
 export default pages
